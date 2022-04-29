bf6d3ace6eebb83bc0ceedca9e6276d268cdb854
EC2 Default User <ec2-user@ip-172-31-5-172.ap-south-1.compute.internal>
2022-04-29 06:03:44 +0000

    b

---
+++ src/test/java/com/social/AppTest.java
@@ -0,0 +1,20 @@
+package com.social;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
